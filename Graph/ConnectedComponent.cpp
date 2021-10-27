0cc6c2d86c949fe8e425922b61a2e025c12cfb80
Amasterworld <73497298+Amasterworld@users.noreply.github.com>
2021-10-27 21:53:40 +0200

    Add files via upload

--- Graph/ConnectedComponent.cpp
+++ Graph/ConnectedComponent.cpp
@@ -1,111 +1,114 @@
+/* find all connected components in a matrix 2D. Connected components are the number 1 that connect together. if only one number 1 is around the number 0. it also is counted as one connected components
+
+for example Matrix = {{1, ,1 ,1 ,1, 1, 1}
+                      {1, ,1 ,0 ,0, 0, 0}         
+                      {0, ,0 ,0 ,0, 0, 0} 
+                      {0, ,0 ,1 ,1, 0, 1}
+                      {0, ,0 ,1 ,1, 0, 0}
+                      {1, ,0 ,0 ,0, 0, 1}}
+
+should return 5
+
+*/
+
+
+/* SOLUTION: use graph to solve this problem, 
+
+
+*/
 #include <iostream>
 #include <vector>
-#include <queue> //FIFO
-#include <cstring>  // for memset
+#include <queue>
+#include <cstring> // for memset
 using namespace std;
+const int n = 6; // given the  size of a matrix n x m
+const int m = 6;
 
-const int n = 5;
-const int m = 5;
+const int dx[] = {0, 1, -1, 0};
+const int dy[] = {1, 0, 0, -1};
  
-// agent can move up down, back and forward
-int dx[] = {0,1,-1,0};  // 0 up, 1 forward, -1 backward, 0 down
-int dy[] = {1,0,0,-1};
-// to check whether visited or not
+ // create a visited array to check whether the cells are visited or not
+
 int visited[n][m];
+//check whether our agent's coors are satisfied,
+// and check the next movement where it is visited
 
-// checkwhether the cell is visited
-bool isVisited(int x_agent, int y_agent, int matrix[n][m]){
-
-    if (x_agent < n and y_agent < m and x_agent >= 0 and y_agent >= 0){
-        if (visited[x_agent][y_agent] == false and matrix[x_agent][y_agent] == 1) return true; //
-        else return false;
-
+bool is_visited(int x, int y, int matrix[n][m]){
+    if (x < n and y< m and x >= 0 and y >= 0){
+        if (matrix[x][y] ==1 and visited[x][y] == false)
+            return true;
+        else 
+            return false;
+    
     }
-    else return false;
-}    
+    else
+        return false;
+
+}
+// we only need to cout, no need return any value so void function is enough
 int count = 0;
+void  FindConnectedComponent(int matrix[n][m]){
 
-void ConnectedComponent(int matrix[n][m]){
-     
-     // to check every cells
-     for (int i = 0; i < n; ++i){
+// search every cell in the matrix
+    for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j){
-            if (visited[i][j] == false and  matrix[i][j] == 1){
+            if (visited[i][j] == false and matrix[i][j] == 1){
                 
-                  // store i and j
-                  queue<pair<int, int> > q;
-                  q.push({i, j});
-                  visited[i][j] = true;
-
+                visited[i][j] = true;
+                // store i and j to check the next adjust cells
+                // to store i and j we can use ordered_map or queue FIFO
+                queue<pair<int, int> > q;
+                q.push({i, j});
+                
+                
                 while (!q.empty()){
-                    pair <int, int> temp = q.front();
+                    pair <int, int> temp_q = q.front();
                     q.pop();
-                    int x = temp.first; int y = temp.second;
-                    for (int i = 0; i< 4; ++i){     // number 4 because the agent only can move up down, left and right.
-                        int newX =   x +  dx[i];
-                        int newY =   y +  dy[i];
-                        if(isVisited(newX, newY, matrix)){
+                    int x = temp_q.first, y = temp_q.second;
+                    for (int i = 0; i < 4; ++i){  // number 4 because our agent only can move  UP DOWN LEFT AND RIGHT
+                        // after this 4 all number 1 around i and j are stored at q
+                        int newX = x + dx[i]; // dx is the possible movement of agent in a grid (for this case is right and left)
+                        int newY = y + dy[i]; // dy is the possible movement of agent in a grid (for this case is UP and DOWN
+
+                        if (is_visited(newX, newY, matrix)){
                             
                             visited[newX][newY] = true;
                             q.push({newX, newY});
- 
-                        }  
+
+
+                        }
+
 
                     }
                 }
-
-                count += 1;
-
+                count +=1;
             }
-                
-
-                
 
 
-           
+
+        }
 
 
-        }       
     }
-    cout <<count <<endl;
 }
 
- int main()
-{
-    // Given input array of 1s and 0s
-   /* int matrix[n][m]
-        = { { 1, 1, 1, 1, 1, 1 }, 
-            { 1, 1, 0, 0, 0, 0 },
-            { 0, 0, 0, 1, 1, 1 }, 
-            { 0, 0, 0, 1, 1, 1 },
-            { 0, 0, 1, 0, 0, 0 }, 
-            { 1, 0, 0, 0, 0, 0 } }; */
 
-    int matrix[n][m] ={{0, 1, 0, 1, 0},
-                       {0, 1, 0, 1, 0},
-                       {0, 1, 1, 1, 0},
-                       {0, 0, 0, 0, 0},
-                       {0, 0, 0, 0, 0}};
- 
-    // queries array
-    //int queries[] = { 6, 1, 8, 2 };
- 
-    // sizeof queries array
-    //int N = sizeof(queries) / sizeof(queries[0]);
- 
-    // Initialize all cells unvisited, ptr(visited) Pointer to the block of memory to fill.
-    // , value (false) - Value to be set. The value is passed as an bool(also can be another type)
-    //, num (Number of bytes to be set to the value.
 
+
+
+int main(){
+
+int matrix[n][m] =      { { 1, 1, 1, 1,  1, 1},
+                        { 1, 1, 0, 0,  0, 0},         
+                        { 0, 0, 0, 0,  0, 0}, 
+                        { 0, 0, 1, 1,  0, 1},
+                        { 0, 0, 1, 1,  0, 0},
+                        { 1, 0, 0, 0,  0, 1} };
+
+    // set all visited array is false
     memset(visited, false, sizeof visited);
- 
-    // Count the frequency of each connected component
-    ConnectedComponent(matrix);
- 
-    // Iterate over the given queries array and
-    // And answer the queries
-   
-    
- 
+
+    FindConnectedComponent(matrix);
+
     return 0;
 }
